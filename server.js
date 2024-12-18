352ccd3d456e1da50ba5cd8fc77709ab69cd35e1
codevil <codevil0209@gmail.com>
2024-12-18 13:04:20 +0530

    added package and server in root

---
+++ server.js
@@ -0,0 +1,21 @@
+
+
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./Config/db');
+const all = require('./Routes/auth'); // Replace the ESModules import
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/auth', require('./Routes/auth'));
+app.use('/api/schools', require('./Routes/school'));
+app.use('/api/classrooms', require('./Routes/classroom'));
+app.use('/api/students', require('./Routes/students'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Hello Chaitanya!!! You server running on port ${PORT}`));
+    
